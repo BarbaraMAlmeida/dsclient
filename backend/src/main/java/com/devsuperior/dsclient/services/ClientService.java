f88c2fb2d5a4011014f4f7e46d08c66020abab0b
Barbara <barbara.nicollealmeida@gmail.com>
2022-02-09 21:50:45 -0300

    Created DTO, seeding database and method findById

--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -1,10 +1,16 @@
 package com.devsuperior.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repository.ClientRepository;
 
@@ -14,7 +20,17 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(item -> new ClientDTO(item)).collect(Collectors.toList());
+
 	}
-}
+
+	@Transactional
+	public ClientDTO findById(long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}  
+} 
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into tb_client (id,birth_date,children,cpf,income,name) values (14, TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 2, '656.876.310-11', 1200, 'Luiza Fernanda');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (3,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 3, '627.151.190-20', 1250, 'Giovanna');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (4, TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 5, '676.785.310-11', 1430, 'Vivian');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (5,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 0, '123.876.310-11', 1300, 'Jeferson');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (6,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 0, '859.876.310-08', 1780,'Davi');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (7,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 0, '589.876.310-07', 800, 'Natalia');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (8,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 0, '656.876.310-11', 5000, 'Nélio');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (9,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 0, '446.265.854-04', 12000, 'Heloisa');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (10, TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 2, '256.876.310-11', 2500, 'Bárbara');
+insert into tb_client (id,birth_date,children,cpf,income,name) values (11,  TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 3,'856.886.318-88', 3400, 'Marcos');
