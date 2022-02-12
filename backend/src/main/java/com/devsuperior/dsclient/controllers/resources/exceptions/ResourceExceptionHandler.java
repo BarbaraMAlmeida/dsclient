c9487d3832457f640e7ffedbaaf69d577651862c
Barbara <barbara.nicollealmeida@gmail.com>
2022-02-11 22:17:13 -0300

    Created post and delete

--- backend/src/main/java/com/devsuperior/dsclient/controllers/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dsclient/controllers/resources/exceptions/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.devsuperior.dsclient.services.exceptions.ClientNotFoundException;
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -21,6 +22,19 @@ public class ResourceExceptionHandler {
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Client not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +14,7 @@ import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repository.ClientRepository;
 import com.devsuperior.dsclient.services.exceptions.ClientNotFoundException;
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
 
 @Service
 public class ClientService {
@@ -33,5 +34,32 @@ public class ClientService {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ClientNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity.setIncome(dto.getIncome());
+		
+		entity = repository.save(entity);
+		
+		return new ClientDTO(entity);
+	}
+
+	public void delete(long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ClientNotFoundException("Id not found "+ id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}  
 } 
